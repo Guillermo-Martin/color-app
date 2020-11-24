7d372a687a9a8a03b22d97f3c8f707ee11b280a4
gcmartin <gscalica@gmail.com>
2020-11-23 20:32:57 -0800

    Add 'react-copy-to-clipboard' as a dependency and implement in ColorBox component

--- src/components/ColorBox/index.js
+++ src/components/ColorBox/index.js
@@ -1,19 +1,23 @@
 import React from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import './ColorBox.css';
 
 function ColorBox(props) {
   const { color, name } = props;
 
   return (
-    <div style={{ background: color }} className="ColorBox">
-      <div className="ColorBox-copy-container">
-        <div className="ColorBox-box-content">
-          <span>{name}</span>
+    <CopyToClipboard text={ color }>
+      <div style={{ background: color }} className="ColorBox">
+        <div className="ColorBox-copy-container">
+          <div className="ColorBox-box-content">
+            <span>{name}</span>
+          </div>
+          <button className="ColorBox-copy-button">Copy</button>
         </div>
-        <button className="ColorBox-copy-button">Copy</button>
+        <span className="ColorBox-more">MORE</span>
       </div>
-      <span className="ColorBox-more">MORE</span>
-    </div>
+    </CopyToClipboard>
+    
   );
 }
 
