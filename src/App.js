5e3824a0a5f49e0e13b80f70c2455735c7694f76
gcmartin <gscalica@gmail.com>
2020-12-22 11:26:20 -0800

    Convert App component to class-based component and fix 'palette' route

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Switch } from "react-router-dom";
 import Palette from './containers/Palette';
 import seedColors from './seedColors';
 import { generatePalette } from './colorHelpers';
 
-function App() {
+class App extends Component {
   // console.log(generatePalette(seedColors[4]));
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/" render={() => <h1>PALETTE LIST GOES HERE</h1>} />
-        <Route exact path="/palette/:id" render={() => <h1>INDIVIDUAL PALETTE GOES HERE</h1>} />
-      </Switch>
-      
-      {/* <div>
-        Pass newly generated palette to Palette component
-        <Palette palette={generatePalette(seedColors[4])} /> 
-      </div> */}
-    </Router>
-    
-  );
+
+  // function to find the correct palette
+  findPalette = id => {
+    return seedColors.find(palette => {
+      // return the palette where the palette's id matches the id passed in
+      return palette.id === id;
+    });
+  }
+
+  render(){
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" render={() => <h1>PALETTE LIST GOES HERE</h1>} />
+  
+          {/* get the id from the path (using 'match.params.id' from react router), and use it to find the right palette with that id inside of our seed colors*/}
+          <Route 
+            exact 
+            path="/palette/:id" 
+            render={routeProps => 
+            <Palette palette={generatePalette(
+              // take the id from the url, find the starter palette in seedColors, then call generate palette, then pass it as props to Palette component
+              this.findPalette(routeProps.match.params.id)
+              )}/> } 
+          />
+        </Switch>
+        
+        {/* <div>
+          Pass newly generated palette to Palette component
+          <Palette palette={generatePalette(seedColors[4])} /> 
+        </div> */}
+      </Router>  
+    );
+  }
 }
 
 export default App;
