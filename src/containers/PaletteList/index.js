01b737b5a004731531b9062af56ccd2f72095dff
gcmartin <gscalica@gmail.com>
2021-03-12 14:00:52 -0800

    Add background svg to PaletteList

--- src/containers/PaletteList/index.js
+++ src/containers/PaletteList/index.js
@@ -21,7 +21,7 @@ class PaletteList extends Component {
         <div className={classes.container}>
           {/* Navbar */}
           <nav className={classes.nav}>
-            <h1>React Colors</h1>
+            <h1 className={classes.heading}>React Colors</h1>
             <Link to="/palette/new">Create Palette</Link>
           </nav>
           {/* Container for MiniPalettes */}
